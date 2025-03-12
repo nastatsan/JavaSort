8c20f7b82f14fb691235a162bda5db844d93c76a
nastatsan <45337772+nastatsan@users.noreply.github.com>
2025-03-12 14:06:56 +0300

    123

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -19,7 +19,7 @@ public class StartMenu {
             int choice = scanner.nextInt();
 
             switch (choice) {
-                case 1:
+                case 123:
                     ForBus.run(args);
                     break;
                 case 2:
